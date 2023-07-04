1f30cd8cf0e89bf21e5ca7177c1353c17e38e96d
MinuMThomas <minumarytomas@gmail.com>
2023-07-04 15:53:12 +0000

    changed allowed host

--- live_plants/settings.py
+++ live_plants/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['liveplants.herokuapp.com', '8000-minumthomas-pp5-a2cr1q16wq3.ws-eu101.gitpod.io', 'localhost']
+ALLOWED_HOSTS = ['liveplants.herokuapp.com', 'localhost']
 
 
 # Application definition
