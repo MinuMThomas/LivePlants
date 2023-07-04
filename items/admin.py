ffb7a09bce211b0c50a87dad82a66ac2ae35e846
MinuMThomas <minumarytomas@gmail.com>
2023-07-04 20:37:39 +0000

    created items-app and its admin page add items.

---
+++ items/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from .models import Item, Category
+
+# Register your models here.
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'description',
+        'price',
+        'image',
+    )
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Category, CategoryAdmin)
