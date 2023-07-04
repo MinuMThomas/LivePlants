324a6b1862ddb248afa35921071918adad38c410
MinuMThomas <minumarytomas@gmail.com>
2023-07-04 16:07:03 +0000

    added new heroku url in allowed hosts.

--- live_plants/settings.py
+++ live_plants/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['liveplants.herokuapp.com', '8000-minumthomas-pp5-a2cr1q16wq3.ws-eu101.gitpod.io', 'localhost']
+ALLOWED_HOSTS = ['liveplants-61cae3ad1da7.herokuapp.com/', '8000-minumthomas-pp5-a2cr1q16wq3.ws-eu101.gitpod.io', 'localhost']
 
 
 # Application definition
