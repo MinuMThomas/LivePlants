cbc6064d869da1f8755d8f615709128322af7d9d
MinuMThomas <minumarytomas@gmail.com>
2023-07-05 23:20:19 +0000

    item detail html added and applied changes in css and index.html.

--- items/views.py
+++ items/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Item
 
 # Create your views here.
@@ -14,3 +14,15 @@ def all_items(request):
     }
 
     return render(request, 'items/items.html', context)
+
+
+def item_detail(request, item_id):
+    """ Shows each item details """
+
+    item = get_object_or_404(Item, pk=item_id)
+
+    context = {
+        'item': item,
+    }
+
+    return render(request, 'items/item_detail.html', context)
