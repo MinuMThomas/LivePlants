095fbd3211db39524677c0a59b0596a8f754e2f5
MinuMThomas <minumarytomas@gmail.com>
2023-07-08 00:26:57 +0000

    Shopping cart function updated in contexts.py.

--- cart/contexts.py
+++ cart/contexts.py
@@ -9,12 +9,24 @@ def cart_contents(request):
     cart_items = []
     total = 0
     item_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        item = get_object_or_404(Item, pk=item_id)
+        total += quantity * item.price
+        item_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'item': item,
+        })
+
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
-        free_delivery = settings.FREE_DELIVERY_THRESHOLD - total
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
-        free_delivery = 0
+        free_delivery_delta = 0
 
     grand_total = delivery + total
 
@@ -23,8 +35,9 @@ def cart_contents(request):
         'total': total,
         'item_count': item_count,
         'delivery': delivery,
-        'free_delivery': free_delivery,
+        'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
     }
+
     return context
