ae580bb2d5fa244a7cb64655dca8119f1d61a59d
MinuMThomas <minumarytomas@gmail.com>
2023-07-09 22:24:40 +0000

    shopping cart page updated with adjust and remove items from cart, also html contents added.

--- cart/views.py
+++ cart/views.py
@@ -34,3 +34,67 @@ def add_to_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+
+def adjust_cart(request, item_id):
+    """Adjust the quantity of the specified item to the specified amount"""
+
+    item = get_object_or_404(Item, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    """ Prevent anyone ordering more than the limit of 99 items or entering /
+        negative values"""
+    if quantity > 99:
+        messages.error(
+            request,
+            f"You cannot have more than 99 { item.name }'s in your cart."
+        )
+
+    elif quantity < 0:
+        messages.error(
+            request,
+            "You cannot add negative quantities. Please remove the item"
+            " using the 'Remove' button or update the number of items."
+        )
+
+    elif quantity > 0:
+        if cart[item_id] + quantity > 99:
+            messages.error(
+                request,
+                f"You cannot have more than 99 { item.name }'s in your cart."
+            )
+        elif cart[item_id] + quantity < 0:
+            messages.error(
+                request,
+                'You cannot add negative quantities. Please remove the item'
+                ' using the "Remove" button or update the number of items.'
+            )
+        else:
+            cart[item_id] = quantity
+            messages.success(
+                request, f'You now have {quantity} x "{item.name}" in your cart')  # noqa
+    else:
+        cart.pop(item_id)
+        messages.success(
+            request, f'You now have {quantity} x "{item.name}" in your cart')
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
+
+def remove_from_cart(request, item_id):
+    """Remove the item from the shopping cart"""
+
+    item = get_object_or_404(Item, pk=item_id)
+    try:
+        cart = request.session.get('cart', {})
+        cart.pop(item_id)
+        messages.success(request, f'"{item.name}" removed from your cart')
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(
+            request, f'An error occured removing "{e}" from your cart')
+        return HttpResponse(status=500)
