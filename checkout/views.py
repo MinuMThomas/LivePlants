35150360f8506fe89a8fbbad59ab27ec2deec471
MinuMThomas <minumarytomas@gmail.com>
2023-07-11 01:24:29 +0000

    Stripe elements added.

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NST6PDebbZ2nd0792Z7A8H45D8iSHWgUkx7en3eM7GjP2nZzopGXTy7lHIDTUMF3V2P812f3xr5bw326M4QZReL00QfbM0m0f',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
