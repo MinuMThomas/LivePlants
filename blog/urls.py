d9dc14443d2d0bb6097c42969c505050c783cec4
MinuMThomas <minumarytomas@gmail.com>
2023-07-14 01:12:54 +0000

    updated blog views, urls and templates.

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-     path('', views.blog_list, name='blog_list')
-    # path('blogs/', views.blog_list, name='blog_list'),
-    # path('', views.BlogListView.as_view(), name='blog_list'),
-    # path('<int:title_id>/', views.blog_detail, name='blog_detail'),
+    path('', views.blog_list, name='blog_list'),
+    path('<int:blog_id>/', views.blog_detail,
+         name='blog_detail'),
 ]
+
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator
 from .models import Blog
 
 
 def blog_list(request):
-    blogs = Blog.objects.all()
-    paginator = Paginator(blogs, per_page=5)  
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    return render(request, 'blog/blog_list.html', {'page_obj': page_obj})
+    """ Shows all blogs """
 
+    blog = Blog.objects.all()
+
+    context = {
+        'blogs': blog,
+    }
+
+    return render(request, 'blog/blog_list.html', context)
+
+
+def blog_detail(request, blog_id):
+    """ Shows individual blog details """
+
+    blog = get_object_or_404(Blog, pk=blog_id)
+
+    context = {
+        'blogs': blog,
+    }
+
+    return render(request, 'blog/blog_detail.html', context)
