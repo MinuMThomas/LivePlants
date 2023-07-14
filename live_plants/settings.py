e77aebf3f08079cedbba8ae8ae9e0e8fe5844691
MinuMThomas <minumarytomas@gmail.com>
2023-07-14 13:12:10 +0000

    Change region name in aws settings.

--- live_plants/settings.py
+++ live_plants/settings.py
@@ -193,7 +193,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 if 'USE_AWS' in os.environ:
     # Bucket config
     AWS_STORAGE_BUCKET_NAME = 'liveplants'
-    AWS_S3_REGION_NAME = 'EU (Stockholm) eu-north-1'
+    AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
